2ff29dfa38ebcbbe89956a1713c142ab44213e60
袁臻 <zenuor@gmail.com>
2017-11-24 19:51:51 +0800

    实现

--- src/main/java/yz/svnotify/Main.java
+++ src/main/java/yz/svnotify/Main.java
@@ -1,90 +1,38 @@
 package yz.svnotify;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+import static yz.svnotify.Utils.getHeadRevision;
+import static yz.svnotify.Utils.getLog;
+import static yz.svnotify.Utils.update;
 
 /**
- * 主类
+ * Main class
  *
- * @author 袁臻
+ * @author yziyz
  * 2017/11/23 16:56
  */
-public class Main {
+public final class Main {
 
-  private static final String COMMAND = "svn %s";
+    /**
+     * Logger instance
+     */
+    private static Logger log = Logger.getLogger(Main.class.getName());
 
-  /**
-   * 主方法
-   *
-   * @param args 命令行参数数组
-   */
-  public static void main(String[] args) throws IOException, InterruptedException {
-    //获取返回值
-    final String argument = arrayToString(args);
-    final SvnCommit svnCommit = SvnCommit.of(argument);
-    log(argument, "/home/user/log");
-    if (svnCommit != null) {
-      log("实例" + svnCommit.toString(), "/home/user/log");
+    /**
+     * Main method
+     *
+     * @param args commandline arguments list
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        update();
+        log.info("Updated");
+        final String revision = getHeadRevision();
+        log.info("Revision: ".concat(revision));
+        final SvnLog svnLog = getLog(revision);
+        log.info(svnLog.toString());
+        final String response = Utils.post(Constants.DING_TALK_URL, String.format(Constants.MESSAGE_TEMPLATE, svnLog.toString()));
+        log.info("Response: \n".concat(response));
     }
-    final int exitCode = exec(String.format(COMMAND, argument));
-    System.exit(exitCode);
-  }
-
-  /**
-   * 将字符串数组转为以空格分隔的字符串
-   *
-   * @param array 字符串数组
-   * @return 以空格分隔的字符串
-   */
-  private static String arrayToString(String[] array) {
-    final StringBuilder stringBuilder = new StringBuilder();
-    for (String s : array) {
-      stringBuilder.append(s).append(' ');
-    }
-    return stringBuilder.deleteCharAt(stringBuilder.length() - 1).toString();
-  }
-
-  /**
-   * 执行命令获取其返回值
-   *
-   * @param argumentString 命令行参数
-   * @return 返回值
-   * @throws IOException 如果IO异常发生
-   */
-  private static int exec(final String argumentString) throws IOException, InterruptedException {
-    //创建执行提交命令的进程
-    final Process process = Runtime.getRuntime().exec(argumentString);
-    //打印输出流
-    final Thread out = new Thread(() -> new BufferedReader(new InputStreamReader(process.getInputStream()))
-            .lines().forEach(line -> {
-              System.out.println(line);
-              try {
-                log(line, "/home/user/out");
-              } catch (IOException e) {
-                e.printStackTrace();
-              }
-            }));
-    out.start();
-    //打印错误流
-    final Thread err = new Thread(() -> new BufferedReader(new InputStreamReader(process.getErrorStream()))
-            .lines().forEach(System.err::println));
-    err.start();
-    //等待线程退出
-    out.join();
-    err.join();
-    //等待进程退出
-    process.waitFor();
-    //返回退出值
-    return process.exitValue();
-  }
-
-  private static void log(String string, String path) throws IOException {
-    final BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(path));
-    bufferedWriter.write(string);
-    bufferedWriter.flush();
-  }
 }
