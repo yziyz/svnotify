95edeec318840cb1bf468fba657142ef7caa5967
袁臻 <zenuor@gmail.com>
2017-11-28 19:26:32 +0800

    重构，修改BUG

--- src/main/java/yz/svnotify/Utils.java
+++ src/main/java/yz/svnotify/Utils.java
@@ -6,10 +6,10 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
 import static yz.svnotify.Constants.JSON;
@@ -26,7 +26,7 @@ final class Utils {
 
     static String post(String url, String json) throws IOException {
         if (url == null) {
-            throw new IllegalArgumentException("Dingtalk URL not found, noew exit.");
+            throw new IllegalArgumentException("Dingtalk URL not found, now exit.");
         }
         RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder()
@@ -57,9 +57,7 @@ final class Utils {
     static String getHeadRevision() throws IOException, InterruptedException {
         //Create a process to execute "svn info --show-item=revision" command
         final Process process = Runtime.getRuntime().exec(
-                Constants.COMMAND_SVN_REVISION,
-                null,
-                new File("/home/user/project/omts")
+                Constants.COMMAND_SVN_REVISION
         );
         //Get STDOUT lines
         final List<String> lines = new BufferedReader(new InputStreamReader(process.getInputStream()))
@@ -77,9 +75,25 @@ final class Utils {
     }
 
     static SvnLog getLog(final String revision) throws IOException {
-        final Process process = Runtime.getRuntime().exec(String.format(Constants.COMMAND_SVN_LOG, revision));
+        final Process process = Runtime.getRuntime().exec(
+                String.format(Constants.COMMAND_SVN_LOG, revision)
+        );
+        final SvnLog svnLog = SvnLog.builder().build();
         final StringBuilder stringBuilder = new StringBuilder();
-        new BufferedReader(new InputStreamReader(process.getInputStream())).lines().forEach(s -> stringBuilder.append(s).append('\n'));
-        return SvnLog.of(stringBuilder.toString());
+        final List<String> stringList = new BufferedReader(new InputStreamReader(process.getInputStream())).lines().collect(Collectors.toList());
+        final Matcher matcher = Constants.PATTERN.matcher(stringList.get(1));
+        if (!matcher.find()) {
+            throw new IllegalStateException("解析日志信息出错");
+        } else {
+            svnLog.setRevision(matcher.group("revision").substring(1));
+            svnLog.setAuthor(matcher.group("author"));
+            svnLog.setTime(matcher.group("time").substring(0, 19));
+        }
+        svnLog.setCommitMessage(stringList.get(stringList.size() - 2));
+        for (int i = 3; i < stringList.size() - 4; i++) {
+            stringBuilder.append(stringList.get(i)).append('\n');
+        }
+        svnLog.setChangedPaths(stringBuilder.toString());
+        return svnLog;
     }
 }
