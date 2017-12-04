a62a47ef65cafed4addff70bc7c22942eeddb085
袁臻 <zenuor@gmail.com>
2017-12-04 11:33:12 +0800

    去除修改文件列表，添加项目名称，优化新建进程方法

--- src/main/java/yz/svnotify/Utils.java
+++ src/main/java/yz/svnotify/Utils.java
@@ -3,19 +3,15 @@ package yz.svnotify;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
-
-import static yz.svnotify.Constants.JSON;
 
 /**
  * 工具类
@@ -25,21 +21,33 @@ import static yz.svnotify.Constants.JSON;
  */
 final class Utils {
 
-    private static final OkHttpClient client = new OkHttpClient();
+    private static final Logger log = Logger.getLogger(Utils.class.getName());
 
-    static String post(String url, String json) throws IOException {
+    /**
+     * HTTP的POST方法
+     *
+     * @param url  统一资源定位符
+     * @param json JSON字符串
+     * @return 响应字符串
+     */
+    static String post(String url, String json) {
         if (url == null) {
             throw new IllegalArgumentException("Dingtalk URL not found, now exit.");
         }
-        RequestBody body = RequestBody.create(JSON, json);
-        Request request = new Request.Builder()
+        final RequestBody body = RequestBody.create(Constants.JSON, json);
+        final Request request = new Request.Builder()
                 .url(url)
                 .post(body)
                 .build();
-        Response response = client.newCall(request).execute();
-        return response.body().string();
+        final ResponseBody responseBody;
+        try {
+            responseBody = new OkHttpClient().newCall(request).execute().body();
+            return responseBody != null ? responseBody.string() : null;
+        } catch (IOException e) {
+            log.warning("POST消息异常");
+            throw new IllegalStateException(e);
+        }
     }
-
 
     static void update() throws IOException, InterruptedException {
         Runtime.getRuntime().exec(
@@ -60,7 +68,9 @@ final class Utils {
     static String getHeadRevision() throws IOException, InterruptedException {
         //Create a process to execute "svn info --show-item=revision" command
         final Process process = Runtime.getRuntime().exec(
-                Constants.COMMAND_SVN_REVISION
+                Constants.COMMAND_SVN_REVISION,
+                null,
+                Constants.WORKING_DIRECTORY
         );
         //Get STDOUT lines
         final List<String> lines = new BufferedReader(new InputStreamReader(process.getInputStream()))
@@ -78,23 +88,37 @@ final class Utils {
     }
 
     static SvnLog getLog(final String revision) throws IOException {
-        final Process process = Runtime.getRuntime().exec(String.format(Constants.COMMAND_SVN_LOG, revision));
+        //创建进程
+        final String command = String.format(Constants.COMMAND_SVN_LOG, revision);
+        log.info("路径:" + Constants.WORKING_DIRECTORY.getAbsolutePath() + "命令:" + command);
+        final Process process = Runtime.getRuntime().exec(
+                command,
+                null,
+                Constants.WORKING_DIRECTORY
+        );
         final SvnLog svnLog = SvnLog.builder().build();
-        final StringBuilder stringBuilder = new StringBuilder();
-        final List<String> stringList = new BufferedReader(new InputStreamReader(process.getInputStream())).lines().collect(Collectors.toList());
-        final Matcher matcher = Constants.PATTERN.matcher(stringList.get(1));
-        if (!matcher.find()) {
+        //获取进程输出字符串列表
+        final List<String> stringList = new BufferedReader(new InputStreamReader(process.getInputStream()))
+                .lines()
+                .collect(Collectors.toList());
+        //匹配日志
+        final Matcher logMatcher = Constants.LOG_PATTERN.matcher(stringList.get(1));
+        if (!logMatcher.find()) {
             throw new IllegalStateException("解析日志信息出错");
         } else {
-            svnLog.setRevision(matcher.group("revision").substring(1));
-            svnLog.setAuthor(matcher.group("author"));
-            svnLog.setTime(matcher.group("time").substring(0, 19));
+            svnLog.setRevision(logMatcher.group("revision").substring(1));
+            svnLog.setAuthor(logMatcher.group("author"));
+            svnLog.setTime(logMatcher.group("time").substring(0, 19));
         }
         svnLog.setCommitMessage(stringList.get(stringList.size() - 2));
-        for (int i = 3; i < stringList.size() - 3; i++) {
-            stringBuilder.append(stringList.get(i)).append('\n');
+        //匹配项目名称
+        if (!stringList.isEmpty()) {
+            final Matcher projectNameMatcher = Constants.PROJECT_NAME_PATTERN.matcher(stringList.get(3));
+            if (projectNameMatcher.find()) {
+                final String projectName = projectNameMatcher.group(1);
+                svnLog.setProjectName(projectName);
+            }
         }
-        svnLog.setChangedPaths(stringBuilder.toString());
         return svnLog;
     }
 }
